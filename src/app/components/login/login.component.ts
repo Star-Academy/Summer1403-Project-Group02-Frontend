d8a4a00c0176d62ca795f7cbf26d73c2677fe65c
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-14 16:11:30 +0330

    feat(style): add login page markup and styles

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TuiInputModule, TuiInputPasswordModule } from '@taiga-ui/legacy';
+import { TuiButton, TuiSurface } from '@taiga-ui/core';
+import { TuiCheckbox } from '@taiga-ui/kit';
+import { TuiCardLarge } from '@taiga-ui/layout';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    TuiInputModule,
+    TuiInputPasswordModule,
+    TuiButton,
+    TuiCheckbox,
+    TuiCardLarge,
+    TuiSurface,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent implements OnInit {
+  form!: FormGroup;
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(32),
+      ]),
+      remember: new FormControl(null, [Validators.required]),
+    });
+  }
+}
