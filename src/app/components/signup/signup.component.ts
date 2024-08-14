ab781247eef94b75d5efbf05309400c73c494bb4
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-14 23:24:21 +0330

    refactor(signup): added new filed (role)

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,25 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { TuiButton, TuiSurface } from '@taiga-ui/core';
+import { TuiButton, TuiDataList, TuiSurface } from '@taiga-ui/core';
+import { TuiDataListWrapper } from '@taiga-ui/kit';
 import { TuiCardLarge } from '@taiga-ui/layout';
-import { TuiInputModule, TuiInputPasswordModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
+import { TuiMultiSelectModule, TuiInputModule, TuiInputPasswordModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [ReactiveFormsModule, TuiCardLarge, TuiSurface, TuiInputModule, TuiTextfieldControllerModule, TuiInputPasswordModule, TuiButton],
+  imports: [ReactiveFormsModule, TuiCardLarge, TuiSurface, TuiInputModule, TuiTextfieldControllerModule, TuiInputPasswordModule, TuiButton, TuiMultiSelectModule, TuiDataListWrapper, TuiDataList],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
 export class SignupComponent implements OnInit {
   signup_form!: FormGroup;
-
+  protected system_role = ["Data Manager", "Analyst"];
   ngOnInit() {
     this.signup_form = new FormGroup({
-      name: new FormControl(null, Validators.required),
-      username: new FormControl(null, [Validators.required, Validators.pattern('^[a-zA-Z][a-zA-Z0-9_-]*$')]),
-      email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, [
+      name: new FormControl<string>('', Validators.required),
+      role: new FormControl<string[]>([], Validators.required),
+      username: new FormControl<string>('', [Validators.required, Validators.pattern('^[a-zA-Z][a-zA-Z0-9_-]*$')]),
+      email: new FormControl<string>('', [Validators.required, Validators.email]),
+      password: new FormControl<string>('', [
         Validators.required,
         Validators.minLength(6),
       ]),
