7530f8cad8bb8634fa8b1f79e4ca4592837d2737
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-18 16:55:13 +0330

    feat(login): integrate login with service

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,5 +12,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     NG_EVENT_PLUGINS,
+    provideHttpClient(),
   ],
 };
