8a4732aafbd8c7b88cff5c428e39fd59399430fa
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-19 14:25:43 +0000

    feat(dashboard): Added logout dialog

--- src/app/components/dashboard/profile/profile.component.ts
+++ src/app/components/dashboard/profile/profile.component.ts
@@ -1,8 +1,8 @@
 import { UpperCasePipe, TitleCasePipe, NgForOf } from '@angular/common';
-import { Component, inject, INJECTOR, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, INJECTOR, OnInit } from '@angular/core';
 import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 import { TuiButton, TuiIcon, TuiTitle, TuiSurface, TuiTextfield, TuiDialogService } from '@taiga-ui/core';
-import { TuiAvatar, TuiButtonLoading, TuiChip, TuiFade } from '@taiga-ui/kit';
+import { TuiAvatar, TuiButtonLoading, TuiChip, TuiFade, TUI_CONFIRM } from '@taiga-ui/kit';
 import { TuiCardLarge } from '@taiga-ui/layout';
 import { AvatarTextPipe } from '../../../pipes/avatar-text.pipe';
 import { UsernamePipe } from '../../../pipes/username.pipe';
@@ -10,14 +10,15 @@ import { RoleAppearancePipe } from '../../../pipes/role-appearance.pipe';
 import { TuiInputModule, TuiTextfieldControllerModule } from '@taiga-ui/legacy';
 import { PolymorpheusComponent } from '@taiga-ui/polymorpheus';
 import { ChangePassDialogComponent } from './change-pass-dialog/change-pass-dialog.component';
-
+import type { TuiConfirmData } from '@taiga-ui/kit';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
   imports: [TuiIcon, TuiButtonLoading, TuiInputModule, TuiTextfield, TuiTextfieldControllerModule, ReactiveFormsModule, TuiSurface, TuiFade, TuiTitle, TuiButton, TitleCasePipe, TuiCardLarge, ReactiveFormsModule, TuiAvatar, UpperCasePipe, UsernamePipe, AvatarTextPipe, RoleAppearancePipe, TuiChip, NgForOf],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrl: './profile.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ProfileComponent implements OnInit {
   protected name = "name family";
@@ -66,6 +67,31 @@ export class ProfileComponent implements OnInit {
         console.info('Dialog closed');
       },
     });
+  }
+
+  protected showLogoutDialog(): void {
+    const data: TuiConfirmData = {
+      content: 'Are you sure you want to logout?',
+      yes: 'Yes',
+      no: 'No',
+    };
+
+    this.dialogs
+      .open<boolean>(TUI_CONFIRM, {
+        label: 'Logout',
+        size: 's',
+        data,
+      })
+      .subscribe({
+        next: (result) => {
+          if (result) {
+            // logout service here
+            console.info('Logout');
+
+            // also redirect to home page
+          }
+        },
+      });
   }
 }
 
