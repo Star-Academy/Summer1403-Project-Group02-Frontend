93bd9726206c31b19188b617e01a631ac9f48403
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-19 12:36:05 +0330

    feat: create change password dialog

---
+++ src/app/components/dashboard/profile/change-pass-dialog/change-pass-dialog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-pass-dialog',
+  standalone: true,
+  imports: [],
+  templateUrl: './change-pass-dialog.component.html',
+  styleUrl: './change-pass-dialog.component.scss'
+})
+export class ChangePassDialogComponent {
+
+}
