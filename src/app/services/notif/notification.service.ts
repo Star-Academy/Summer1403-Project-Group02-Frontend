a5e840eb661a34050902676d17b7c4035836a135
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-08-21 20:13:41 +0330

    reformat: delete comments

--- src/app/services/notif/notification.service.ts
+++ src/app/services/notif/notification.service.ts
@@ -7,52 +7,26 @@ import { TuiAlertService, TuiAlertContext } from '@taiga-ui/core';
 export class NotificationService {
   constructor(private alertService: TuiAlertService) {}
 
-  /**
-   * Show a success notification.
-   * @param message The message to display.
-   * @param label Optional label for the notification.
-   */
   showSuccess(message: string, label = 'Success'): void {
     this.alertService
       .open(message, { label, appearance: 'success' })
       .subscribe();
   }
 
-  /**
-   * Show an error notification.
-   * @param message The message to display.
-   * @param label Optional label for the notification.
-   */
   showError(message: string, label = 'Error'): void {
     this.alertService.open(message, { label, appearance: 'error' }).subscribe();
   }
 
-  /**
-   * Show a warning notification.
-   * @param message The message to display.
-   * @param label Optional label for the notification.
-   */
   showWarning(message: string, label = 'Warning'): void {
     this.alertService
       .open(message, { label, appearance: 'warning' })
       .subscribe();
   }
 
-  /**
-   * Show an information notification.
-   * @param message The message to display.
-   * @param label Optional label for the notification.
-   */
   showInfo(message: string, label = 'info'): void {
     this.alertService.open(message, { label, appearance: 'info' }).subscribe();
   }
 
-  /**
-   * Show a custom template notification.
-   * @param template The template to display.
-   * @param label Optional label for the notification.
-   * @param appearance The appearance style (success, warning, etc.)
-   */
   showTemplate(
     template: TemplateRef<TuiAlertContext>,
     label = 'Notification',
