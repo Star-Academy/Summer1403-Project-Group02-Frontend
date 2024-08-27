88f2fbb5091faa59a39e95fe1cb1c2386f3c9ce0
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-08-27 15:44:21 +0330

    feat: added tab in charts component

--- src/app/components/dashboard/charts/charts.component.ts
+++ src/app/components/dashboard/charts/charts.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { TuiTabs } from '@taiga-ui/kit';
+
+import { GraphComponent } from '../graph/graph.component';
+import { ImportComponent } from '../import/import.component';
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [],
+  imports: [GraphComponent, ImportComponent, TuiTabs, FormsModule],
   templateUrl: './charts.component.html',
-  styleUrl: './charts.component.scss'
+  styleUrl: './charts.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ChartsComponent {
+  protected activeItemIndex = 0;
 
+  protected tabHandler(item: string): void {
+    // if (item == "import") {
+    //   this.activeItemIndex = 0;
+    // }
+
+    // else if (item == "graph") {
+    //   this.activeItemIndex = 1;
+    // }
+
+    // else {
+    //   this.activeItemIndex = 0;
+    // }
+  }
 }
