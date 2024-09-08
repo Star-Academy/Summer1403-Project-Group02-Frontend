62ee31ef38d45c803934f69fbf00bde38c346391
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-09 02:34:01 +0330

    chore(change pass): remove sub title

--- src/app/components/dashboard/profile/change-pass-dialog/change-pass-dialog.component.ts
+++ src/app/components/dashboard/profile/change-pass-dialog/change-pass-dialog.component.ts
@@ -59,6 +59,9 @@ export class ChangePassDialogComponent {
         complete: () => {
           this.loading = false;
         },
+        error: () => {
+          this.loading = false;
+        },
       });
     }
   }
