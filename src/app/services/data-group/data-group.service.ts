825a36a086916a2862640079c0e462ad440ae410
Morteza Ghandchi <mortezaghandchi7@gmail.com>
2024-09-09 13:35:17 +0330

    fix: update services

--- src/app/services/data-group/data-group.service.ts
+++ src/app/services/data-group/data-group.service.ts
@@ -1,7 +1,10 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 
+// Define the original data structure (the expected format)
 export interface DataGroup {
   dataGroupId: number;
   name: string;
@@ -13,11 +16,25 @@ export interface DataGroup {
   providedIn: 'root',
 })
 export class DataGroupService {
-  private apiUrl = 'http://127.0.0.1:8000/data-groups';
+  // API endpoint (replace this with the actual FastAPI endpoint)
+  private apiUrl = `${environment.apiBaseUrl}/DataSets`;
 
   constructor(private http: HttpClient) {}
 
+  // Method to get data groups from the backend and map to the old format
   getDataGroups(): Observable<DataGroup[]> {
-    return this.http.get<DataGroup[]>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl).pipe(
+      map((response) => {
+        console.log(response);
+
+        // Map the new API response to the old format
+        return response.data.map((item: any) => ({
+          dataGroupId: item.id, // map `id` to `dataGroupId`
+          name: item.name, // map `name` to `name`
+          createAt: item.createAt, // map `createAt` to `createAt`
+          updateAt: item.updateAt, // map `updateAt` to `updateAt`
+        }));
+      })
+    );
   }
 }
