115a4ec94bbc8e297829b1124dafdacc7484306b
Mr MRF <79112136+Mr-MRF-Dev@users.noreply.github.com>
2024-09-09 11:49:30 +0330

    fix(chart): fix test

--- src/app/components/dashboard/charts/charts.component.spec.ts
+++ src/app/components/dashboard/charts/charts.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ChartsComponent } from './charts.component';
 
@@ -8,9 +9,8 @@ describe('ChartsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ChartsComponent]
-    })
-    .compileComponents();
+      imports: [BrowserAnimationsModule, ChartsComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ChartsComponent);
     component = fixture.componentInstance;
